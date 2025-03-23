37b9f45f76bbfddfbf104d0cf35d481827fbe829
Giulia Bondi Mazzoni <giulia.bondimazzoni@gmail.com>
2025-03-23 15:41:53 +0100

    Dommain class repositories added

--- src/main/java/guru/springframework/spring6webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring6webapp/repositories/AuthorRepository.java
@@ -1,4 +1,7 @@
 package guru.springframework.spring6webapp.repositories;
 
-public interface AuthorRepository {
+import guru.springframework.spring6webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
